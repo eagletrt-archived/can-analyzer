3c93e7b7174c3a9567e9036816ceb7659e0d5454
mattebit <matteobit@hotmail.com>
2019-07-11 17:08:50 +0200

    Adjusted routes & added includes

--- simulator/server-node/server.js
+++ simulator/server-node/server.js
@@ -4,13 +4,18 @@ const path = require('path');
 const router = express.Router();
 
 app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/views/'));
+//app.use(express.static(__dirname + '/views/icons/'));
+//pp.use(express.static(__dirname + '/views/images/'));
+//app.use(express.static(__dirname + '/views/src/'));
 
 router.get('/',function(req,res){
   res.render('index.ejs');
-  //__dirname : It will resolve to your project folder.
 });
 
-//Da fare il routing di tutte le foto e i file
+router.get('/car_view.ejs',function(req,res){
+  res.render('car_view.ejs');
+});
 
 //add the router
 app.use('/', router);
